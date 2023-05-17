0eb0390f39e02d7444d0204d5f800eedaa92f52c
teh42eem00 <tomasz.macalka@student.pk.edu.pl>
2023-05-18 00:48:47 +0200

    Scraping trips, generating trip hashes, adding new trips or updating existing ones

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
+import hashlib
 from functools import wraps
 from flask import Flask, render_template, request, redirect, url_for, session
+from bs4 import BeautifulSoup
+import re
 import sqlite3
 import bcrypt
+import requests
 
 app = Flask(__name__)
 app.secret_key = 'SomeSecretKey!1'
-
-# Configure SQLite database
 DATABASE = 'database.db'
 
 
@@ -22,18 +24,14 @@ def login_required(route_function):
     return wrapper
 
 
+# Create DB
 def create_database():
     conn = sqlite3.connect(DATABASE)
     c = conn.cursor()
-
-    # Create table for users
     c.execute('''CREATE TABLE IF NOT EXISTS users
                  (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)''')
-
-    # Create table for trips
     c.execute('''CREATE TABLE IF NOT EXISTS trips
-                 (id INTEGER PRIMARY KEY AUTOINCREMENT, destination TEXT, price INTEGER)''')
-
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, location TEXT, days TEXT, price INTEGER, last_price INTEGER, departure_location TEXT, trip_hash TEXT)''')
     conn.commit()
     conn.close()
 
@@ -51,6 +49,7 @@ def index():
 @app.route('/trips')
 @login_required
 def trips():
+    scrape_and_load_offers()  # Scrape and load offers into the database
     conn = sqlite3.connect(DATABASE)
     c = conn.cursor()
     c.execute('SELECT * FROM trips')
@@ -82,7 +81,6 @@ def register():
 
 
 # Route for user login
-# Route for user login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -99,21 +97,67 @@ def login():
             stored_password = row[0]
 
             if bcrypt.checkpw(password.encode('utf-8'), stored_password):
-                session['username'] = username  # Store the username in the session
-                return redirect(url_for('trips'))  # Redirect to 'trips' endpoint
+                session['username'] = username
+                return redirect(url_for('trips'))
 
         return 'Invalid username or password'
 
     return render_template('index.html')
 
 
-
-# Route for loggin out
+# Route for logging out
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
     # Clear session data or perform any necessary logout operations
     session.clear()
     return redirect(url_for('login'))
+
+
+# Scrapes the website for holiday offers and loads them into the database
+def scrape_and_load_offers():
+    print("Starting scraping...")
+    url = 'https://r.pl/szukaj?wybraneSkad=KTW&wybraneSkad=KRK&typTransportu=AIR&data=2023-06-24&data=2023-07-30&dorosli=1992-01-01&dorosli=1992-01-01&dzieci=2020-05-23&dzieci=2012-08-22&liczbaPokoi=1&dowolnaLiczbaPokoi=nie&wyzywienia=all-inclusive&wyzywienia=3-posilki&wyzywienia=2-posilki&dlugoscPobytu=*-*&dlugoscPobytu.od=8&dlugoscPobytu.do=&odlegloscLotnisko=*-*&cena=sum&cena.od=&cena.do=&ocenaKlientow=&sortowanie=cena-asc'
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    conn = sqlite3.connect(DATABASE)
+    c = conn.cursor()
+
+    trip_divs = soup.findAll("div", class_="r-card__body")
+    for trip_div in trip_divs:
+        title = trip_div.find("span", class_="r-bloczek-tytul").text.strip()
+        location = trip_div.find("span", class_="r-bloczek-lokalizacja").text.strip()
+        days = trip_div.find("div", class_="r-bloczek-wlasciwosc__dni").text.strip()
+        price = trip_div.find("div", class_="r-bloczek-cena").text.strip()
+        departure_location = trip_div.find("span", class_="r-bloczek-przystanki__info").previous_sibling.strip()
+
+        price = re.sub(r'\D', '', price)
+        days = days.split(')')[0].strip().replace("(", "- ")
+
+        # Generate trip hash
+        trip_hash = hashlib.md5((title + location + days + departure_location).encode('utf-8')).hexdigest()
+
+        # Check if trip already exists in the database
+        c.execute('SELECT price FROM trips WHERE trip_hash = ?', (trip_hash,))
+        row = c.fetchone()
+
+        if row:
+            stored_price = row[0]
+
+            if int(price) != stored_price:
+                # Update the price and set the previous price as the last price
+                c.execute('UPDATE trips SET price = ?, last_price = ? WHERE trip_hash = ?',
+                          (int(price), stored_price, trip_hash))
+        else:
+            # Insert the new trip into the database
+            c.execute(
+                'INSERT INTO trips (title, location, days, price, last_price, departure_location, trip_hash) VALUES (?, ?, ?, ?, ?, ?, ?)',
+                (title, location, days, int(price), int(price), departure_location, trip_hash))
+
+    conn.commit()
+    conn.close()
+
+    print("Scraping completed!")
 
 
 if __name__ == '__main__':
