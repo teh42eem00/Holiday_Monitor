e42da34cd79522e8ace094bfe22fbefa93ea0b96
teh42eem00 <85800518+teh42eem00@users.noreply.github.com>
2024-07-23 16:49:46 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+import sqlite3
 from bs4 import BeautifulSoup
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
@@ -5,11 +6,8 @@ from email_utils import send_email
 from file_db_utils import load_urls_from_file, DATABASE
 import hashlib
 import re
-import sqlite3
 import requests
 
-
-# Scrapes the website for holiday offers and loads them into the database
 def scrape_and_load_offers():
     print("Starting scraping...")
 
@@ -19,10 +17,6 @@ def scrape_and_load_offers():
         c.execute('SELECT * FROM subscriptions')
         subscriptions = c.fetchall()
 
-        # Retrieve the existing trip hashes from the database
-        # c.execute('SELECT trip_hash FROM trips')
-        # hashes_to_remove = [row[0] for row in c.fetchall()]
-
         for url in urls:
             response = requests.get(url)
             soup = BeautifulSoup(response.content, 'html.parser')
@@ -31,88 +25,51 @@ def scrape_and_load_offers():
             for trip in trips:
                 title = trip.find("span", class_="r-bloczek-tytul").text.strip()
                 location = trip.find("span", class_="r-bloczek-lokalizacja").text.strip()
-                days = trip.find("div", class_="r-bloczek-wlasciwosci__dni").text.strip().split(')')[0].strip().replace("(",
-                                                                                                                       "- ")
+                days = trip.find("div", class_="r-bloczek-wlasciwosci__dni").text.strip().split(')')[0].strip().replace("(", "- ")
                 price = trip.find("span", class_="r-bloczek-cena__aktualna").text.strip()
-                departure_location = trip.find("div", class_="r-bloczek-wlasciwosci__dni").find_next('div',
-                                                                                                    class_='r-bloczek-wlasciwosci__wlasciwosc').text.strip()
+                departure_location = trip.find("div", class_="r-bloczek-wlasciwosci__dni").find_next('div', class_='r-bloczek-wlasciwosci__wlasciwosc').text.strip()
                 food = trip.find("span", class_="r-bloczek-wyzywienie__nazwa").text.strip()
 
                 href = trip['href']
-                if not href.startswith("http"):
-                    trip_url = "https://r.pl" + href
-                else:
-                    trip_url = href
-                    
+                trip_url = "https://r.pl" + href if not href.startswith("http") else href
+                
                 adults_count, children_count = 0, 0
-                # Extracting the number of adults and children from the URL
                 if "dorosli" in url:
                     adults_count = url.count("dorosli")
                 if "dzieci" in url:
                     children_count = url.count("dzieci")
 
-                # Calculate the formatted persons string
                 persons_formatted = f"{adults_count} adults, {children_count} children"
+                price = int(re.sub(r'\D', '', price))
 
-                # Clean and format the price
-                price = re.sub(r'\D', '', price)
+                trip_hash = hashlib.md5((title + location + days + departure_location + food + persons_formatted).encode('utf-8')).hexdigest()
 
-                # Generate trip hash
-                trip_hash = hashlib.md5(
-                    (title + location + days + departure_location + food + persons_formatted).encode('utf-8')).hexdigest()
-
-                # Check if trip already exists in the database
                 c.execute('SELECT price FROM trips WHERE trip_hash = ?', (trip_hash,))
                 row = c.fetchone()
 
                 if row:
                     stored_price = row[0]
-                    # hashes_to_remove.remove(trip_hash)
-                    if int(price) != stored_price:
-                        # Update the price and set the previous price as the last price
-                        c.execute('UPDATE trips SET price = ?, last_price = ?, food = ?, persons = ? WHERE trip_hash = ?',
-                                  (int(price), int(stored_price), food, persons_formatted, trip_hash))
+                    if price != stored_price:
+                        c.execute('UPDATE trips SET price = ?, last_price = ?, food = ?, persons = ? WHERE trip_hash = ?', (price, stored_price, food, persons_formatted, trip_hash))
+                        c.execute('INSERT INTO price_history (trip_hash, price) VALUES (?, ?)', (trip_hash, price))
                 else:
-                    # Insert the new trip into the database
-                    c.execute(
-                        'INSERT INTO trips (title, location, days, price, last_price, departure_location, food, persons, trip_hash, trip_url) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                        (title, location, days, int(price), int(price), departure_location, food, persons_formatted,
-                         trip_hash, trip_url))
+                    c.execute('INSERT INTO trips (title, location, days, price, last_price, departure_location, food, persons, trip_hash, trip_url) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', (title, location, days, price, price, departure_location, food, persons_formatted, trip_hash, trip_url))
+                    c.execute('INSERT INTO price_history (trip_hash, price) VALUES (?, ?)', (trip_hash, price))
 
-                # Process subscriptions for matching trips
                 for subscription in subscriptions:
                     email = subscription[1]
                     desired_price = subscription[2]
 
-                    # Check if the trip matches the subscription criteria and has not been alerted before
-                    c.execute(
-                        'SELECT * FROM subscription_trips WHERE subscription_email = ? AND trip_hash = ? AND trip_price = ?',
-                        (email, trip_hash, price))
-
+                    c.execute('SELECT * FROM subscription_trips WHERE subscription_email = ? AND trip_hash = ? AND trip_price = ?', (email, trip_hash, price))
                     row = c.fetchone()
 
-                    if int(price) <= desired_price and not row:
-                        # Prepare the email content
+                    if price <= desired_price and not row:
                         subject = 'Trip Alert: Price Match!'
                         content = f"There is a trip that matches your criteria:\n\nTitle: {title}\nLocation: {location}\nPrice: {price}\n\nYou can book the trip at: {trip_url}"
-
-                        # Send the email
                         send_email(subject, content, email)
-
-                        # Insert the trip hash and price into the subscription_trips table
-                        c.execute(
-                            'INSERT INTO subscription_trips (subscription_email, trip_hash, trip_price) VALUES (?, ?, ?)',
-                            (email, trip_hash, int(price)))
-
-                        # Delete trips with hashes that are not found on the scraped URLs anymore
-                        # Uncomment the following code to delete the trips
-                        """
-                        for hash_to_delete in hashes_to_remove:
-                            c.execute('DELETE FROM trips WHERE trip_hash = ?', (hash_to_delete,))
-                        """
+                        c.execute('INSERT INTO subscription_trips (subscription_email, trip_hash, trip_price) VALUES (?, ?, ?)', (email, trip_hash, price))
 
     print("Scraping completed!")
-
 
 def schedule_scraping():
     print("Scraping scheduled...")
