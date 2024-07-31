db8121180b5446e732203db5ba183b9852a4a3cb
teh42eem00 <85800518+teh42eem00@users.noreply.github.com>
2024-07-31 22:15:50 +0200

    Update file_db_utils.py

--- file_db_utils.py
+++ file_db_utils.py
@@ -2,25 +2,24 @@ import sqlite3
 
 DATABASE = 'database.db'
 
-
-# Create DB
 def create_database():
     conn = sqlite3.connect(DATABASE)
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS users
                  (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)''')
     c.execute('''CREATE TABLE IF NOT EXISTS trips
-                 (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, location TEXT, days TEXT, price INTEGER, last_price INTEGER, departure_location TEXT, food TEXT, persons TEXT, trip_hash TEXT, trip_url TEXT)''')
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, location TEXT, days TEXT, price INTEGER, last_price INTEGER, departure_location TEXT, food TEXT, persons TEXT, review_score TEXT, trip_hash TEXT, trip_url TEXT)''')
     c.execute('''CREATE TABLE IF NOT EXISTS subscriptions
                  (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT UNIQUE, desired_price INTEGER)''')
     c.execute('''CREATE TABLE IF NOT EXISTS subscription_trips
                  (id INTEGER PRIMARY KEY AUTOINCREMENT, subscription_email TEXT, trip_hash TEXT, trip_price INTEGER)''')
+    c.execute('''CREATE TABLE IF NOT EXISTS price_history
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT, trip_hash TEXT, date TEXT, price INTEGER)''')
     c.execute('''CREATE TABLE IF NOT EXISTS price_changes
                  (id INTEGER PRIMARY KEY AUTOINCREMENT, trip_hash TEXT, title TEXT, location TEXT, date TEXT, current_price INTEGER, previous_price INTEGER, departure_location TEXT, food TEXT, persons TEXT, change_date TEXT)''')
     conn.commit()
     conn.close()
 
-# Function to loads urls to scrap from file
 def load_urls_from_file():
     with open("static/urls.txt", "r") as file:
-        return [line.strip() for line in file]
+        return [line.strip() for line]
